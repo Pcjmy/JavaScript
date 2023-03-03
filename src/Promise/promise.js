d125a2a9ca5bbe5a08cea68dcdb19135726668d6
Pcjmy <1716045191@qq.com>
2023-03-03 16:44:40 +0800

    feat: Promise then

--- src/Promise/promise.js
+++ src/Promise/promise.js
@@ -1,12 +1,15 @@
-const PENDING = 'pending'
-const FUFILLED = 'fufilled'
-const REJECTED = 'rejected'
+const PENDING = 'pending';
+const FULFILLED = 'fufilled';
+const REJECTED = 'rejected';
 
 class MyPromise {
+  FULFILLED_CALLBACK_LIST = [];
+  REJECTED_CALL_LIST = [];
+
   constructor(fn) {
-    this.status = PENDING
-    this.value = null
-    this.reson = null
+    this.status = PENDING;
+    this.value = null;
+    this.reson = null;
 
     try {
       fn(this.resolve.bind(this), this.reject.bind(this))
@@ -17,7 +20,7 @@ class MyPromise {
 
   resolve(value) {
     if (this.status === PENDING) {
-      this.status = FUFILLED
+      this.status = FULFILLED
       this.value = value
     }
   }
@@ -28,4 +31,35 @@ class MyPromise {
       this.reason = reason
     }
   }
+
+  then(onFulfilled, onRejected) {
+    const fulFilledFn = this.isFunction(onFulfilled) ? onFulfilled : (value) => {
+      return value;
+    }
+    const rejectedFn = this.isFunction(onRejected) ? onRejected: (reason) => {
+      throw reason;
+    }
+
+    switch(this.status) {
+      case FULFILLED: {
+        fulFilledFn(this.value);
+        break;
+      }
+      case REJECTED: {
+        rejectedFn(this.reason);
+        break;
+      }
+      case PENDING: {
+        this.FULFILLED_CALLBACK_LIST.push(fulFilledFn);
+        this.REJECTED_CALL_LIST.push(rejectedFn);
+        break;
+      }
+    }
+  }
+
+  isFunction(param) {
+    return typeof param === 'function';
+  }
 }
+
+const promise = new MyPromise((resolve, reject) => {})
