a6cb126cc58ba1607fc7c006bcb18f3907c6262c
Pcjmy <1716045191@qq.com>
2023-04-13 15:36:34 +0800

    feat: 设计模式

---
+++ src/DesignPattern/DesignPattern.ts
@@ -0,0 +1,20 @@
+class SingleTon {
+  private static instance: SingleTon | null = null
+  private constructor() {}
+  public static getInstance(): SingleTon {
+    if (this.instance == null) {
+      this.instance = new SingleTon()
+    }
+    return this.instance
+  }
+  fn1() {}
+  fn2() {}
+}
+
+const s = SingleTon.getInstance()
+s.fn1()
+s.fn2()
+
+const s1 = SingleTon.getInstance()
+
+s===s1 // true
