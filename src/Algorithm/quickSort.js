5d9bcfd44547126a6eb78ebd3b2e16accb1115f3
Pcjmy <1716045191@qq.com>
2023-04-29 22:30:12 +0800

    feat: quickSort

--- src/Algorithm/quickSort.js
+++ src/Algorithm/quickSort.js
@@ -4,13 +4,13 @@ function quickSort(arr) {
   let right = [];
   let x = arr[0];
   for (let i=1; i<arr.length; i++) {
-    if (arr[i]<x) {
+    if (arr[i] < x) {
       left.push(arr[i])
     } else {
       right.push(arr[i])
     }
   }
-  return quickSort(left).concat(arr[0],quickSort(right))
+  return quickSort(left).concat(arr[0], quickSort(right))
 }
 
 console.log(quickSort([2,7,3,1,6,5,4,9,8]))
