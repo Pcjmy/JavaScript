2d8caa2282b14ae7adec768ca59d5d7b6a8844ca
Pcjmy <1716045191@qq.com>
2023-10-04 22:48:59 +0800

    feat: websocket建立连接

--- src/WebSocket/server.js
+++ src/WebSocket/server.js
@@ -4,29 +4,30 @@ const crypto = require('crypto');
 const server = net.createServer((socket) => {
   console.log('connected');
 
-  socket.on('data', (buffer) => {
+  socket.once('data', (buffer) => {
     console.log('data');
     const headers = parseHttpHeaders(buffer);
-    let first = true;
-    if (first) {
-      if (headers.upgrade !== 'websocket') {
-        socket.end();
-      } else if (headers['sec-websocket-version'] !== '13') {
-        socket.end();
-      } else {
-        // 258EAFA5-E914-47DA-95CA-C5AB0DC85B11
-        // accept = sha1(key+mask).base64()
-        let key = headers['sec-websocket-key'];
-        const mask = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-        let hash = crypto.createHash('sha1');
-        hash.update(key + mask);
-        let accept = hash.digest('base64');
-        socket.write(`HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${accept}\r\n\r\n`);
-      }
-      first = false;
+
+    if (headers.upgrade !== 'websocket') {
+      socket.end();
+    } else if (headers['sec-websocket-version'] !== '13') {
+      socket.end();
+    } else {
+      // 258EAFA5-E914-47DA-95CA-C5AB0DC85B11
+      // accept = sha1(key+mask).base64()
+      let key = headers['sec-websocket-key'];
+      const mask = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+      let hash = crypto.createHash('sha1');
+      hash.update(key + mask);
+      let accept = hash.digest('base64');
+      socket.write(`HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${accept}\r\n\r\n`);
     }
 
     console.log(headers);
+
+    socket.on('data', (buffer) => {
+      console.log(buffer);
+    })
   });
 
   socket.on('end', () => {
