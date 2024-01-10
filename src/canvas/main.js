1d569806293dec6901b7c8f997582c03efe60af6
Pcjmy <1716045191@qq.com>
2024-01-10 22:41:51 +0800

    feat: canvas绘制弧形

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -8,8 +8,15 @@ console.log(ctx);
 // ctx.fillRect(100, 100, 100, 100);
 // ctx.strokeRect(300, 300, 100, 100);
 
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(50, 100);
-ctx.lineTo(50,50);
+// canvas绘制三角形
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(50, 100);
+// ctx.lineTo(50,50);
+// ctx.stroke();
+
+// canvas绘制弧形
+// arc(x, y, radius, startAngle, endAngle, anticlockwise)
+ctx.beginPath();
+ctx.arc(200, 200, 50, 0, 2 * Math.PI);
 ctx.stroke();
