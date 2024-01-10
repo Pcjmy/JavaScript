aaf9bfb0e3400acc001e8453a3a7abf408a02467
Pcjmy <1716045191@qq.com>
2024-01-10 22:12:23 +0800

    feat: getContext

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -1,3 +1,9 @@
 const canvas = document.getElementById('myCanvas');
-canvas.style.backgroundColor = 'red';
+canvas.style.border = '2px solid black';
 console.log(canvas);
+
+const ctx = canvas.getContext('2d');
+console.log(ctx);
+
+ctx.fillRect(100, 100, 100, 100);
+ctx.strokeRect(300, 300, 100, 100);
