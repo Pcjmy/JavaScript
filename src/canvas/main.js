2f2e64e81718bd7f1c70df5b11ce36bed07fe196
Pcjmy <1716045191@qq.com>
2024-01-13 22:26:26 +0800

    feat: canvas实现方形图标

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -11,3 +11,15 @@ ctx.arc(90, 100, 10, 0, 2 * Math.PI);
 ctx.moveTo(90 + 10 * Math.sin(Math.PI / 4), 100 + 10 * Math.sin(Math.PI / 4));
 ctx.lineTo(106, 116);
 ctx.stroke();
+
+
+ctx.moveTo(200, 100);
+ctx.lineTo(220, 100);
+ctx.arc(220, 110, 10, 3 / 2 * Math.PI, 2 * Math.PI);
+ctx.lineTo(230, 130);
+ctx.arc(220, 130, 10, 0, Math.PI / 2);
+ctx.lineTo(200, 140);
+ctx.arc(200, 130, 10, Math.PI / 2, Math.PI);
+ctx.lineTo(190, 110);
+ctx.arc(200, 110, 10, Math.PI, 3 / 2 * Math.PI);
+ctx.stroke();
