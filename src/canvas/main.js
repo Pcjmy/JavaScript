37b1576664b341b4daf347b9f3a1ebc010676c0d
Pcjmy <1716045191@qq.com>
2024-01-13 23:51:12 +0800

    feat: canvas实现签字版效果

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -1,25 +1,42 @@
 const canvas = document.getElementById('myCanvas');
-canvas.width = 500;
 canvas.height = 500;
+canvas.width = 500;
 canvas.style.border = '1px solid black';
 
 const ctx = canvas.getContext('2d');
-ctx.moveTo(100, 100);
-ctx.lineWidth = 5;
-ctx.lineCap = 'round';
-ctx.arc(90, 100, 10, 0, 2 * Math.PI);
-ctx.moveTo(90 + 10 * Math.sin(Math.PI / 4), 100 + 10 * Math.sin(Math.PI / 4));
-ctx.lineTo(106, 116);
-ctx.stroke();
 
+const prePos = {
+  x: 0,
+  y: 0
+}
 
-ctx.moveTo(200, 100);
-ctx.lineTo(220, 100);
-ctx.arc(220, 110, 10, 3 / 2 * Math.PI, 2 * Math.PI);
-ctx.lineTo(230, 130);
-ctx.arc(220, 130, 10, 0, Math.PI / 2);
-ctx.lineTo(200, 140);
-ctx.arc(200, 130, 10, Math.PI / 2, Math.PI);
-ctx.lineTo(190, 110);
-ctx.arc(200, 110, 10, Math.PI, 3 / 2 * Math.PI);
-ctx.stroke();
+let writeable = false;
+
+canvas.addEventListener('mousemove', (e) => {
+  if (writeable) {
+    draw(e.offsetX, e.offsetY);
+  }
+})
+
+canvas.addEventListener('mousedown', (e) => {
+  writeable = true;
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  writeable = false;
+  prePos.x = 0;
+  prePos.y = 0;
+})
+
+function draw(x, y) {
+  if (prePos.x === 0 && prePos.y === 0) {
+    prePos.x = x;
+    prePos.y = y;
+  } else {
+    ctx.moveTo(prePos.x, prePos.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    prePos.x = x;
+    prePos.y = y;
+  }
+}
