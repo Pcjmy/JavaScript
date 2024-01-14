c676b605011f4f591e8cfab25f5c93f5bd8f1a27
Pcjmy <1716045191@qq.com>
2024-01-14 21:27:20 +0800

    feat: canvas坐标系变化

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -1,42 +1,21 @@
 const canvas = document.getElementById('myCanvas');
-canvas.height = 500;
-canvas.width = 500;
+canvas.width = 600;
+canvas.height = 600;
 canvas.style.border = '1px solid black';
 
 const ctx = canvas.getContext('2d');
+ctx.lineWidth = 6;
+ctx.lineCap = 'round';
+ctx.moveTo(220, 200);
+ctx.arc(200, 200, 20, 0, 2 * Math.PI);
+ctx.stroke();
 
-const prePos = {
-  x: 0,
-  y: 0
-}
+ctx.translate(200, 200);
+ctx.rotate(-(Math.PI / 180) * 90);
 
-let writeable = false;
-
-canvas.addEventListener('mousemove', (e) => {
-  if (writeable) {
-    draw(e.offsetX, e.offsetY);
-  }
-})
-
-canvas.addEventListener('mousedown', (e) => {
-  writeable = true;
-})
-
-canvas.addEventListener('mouseup', (e) => {
-  writeable = false;
-  prePos.x = 0;
-  prePos.y = 0;
-})
-
-function draw(x, y) {
-  if (prePos.x === 0 && prePos.y === 0) {
-    prePos.x = x;
-    prePos.y = y;
-  } else {
-    ctx.moveTo(prePos.x, prePos.y);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    prePos.x = x;
-    prePos.y = y;
-  }
+for (let i = 0; i < 8; i++) {
+  ctx.moveTo(30, 0);
+  ctx.lineTo(50,0);
+  ctx.stroke();
+  ctx.rotate((Math.PI / 180) * 45);
 }
