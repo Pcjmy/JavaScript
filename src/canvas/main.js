46d8e3061780703e9bada76369a906246fb92da4
Pcjmy <1716045191@qq.com>
2024-01-18 00:29:17 +0800

    feat: canvas实现苹果官网耳机效果

--- src/canvas/main.js
+++ src/canvas/main.js
@@ -7,7 +7,10 @@ canvas.style.backgroundColor = 'black';
 
 const ctx = canvas.getContext('2d');
 
+const imgsArr = [];
+
 function calcDrawStartPos(img) {
+  console.log(img.width, img.height);
   const x = (canvas.width - img.width) / 2;
   const y = (canvas.height - img.height) / 2;
   return {
@@ -15,14 +18,34 @@ function calcDrawStartPos(img) {
   }
 }
 
-const img1 = new Image();
-img1.src = './assets/0001.png';
-
-img1.onload = (e) => {
-  const startPos = calcDrawStartPos(e.target);
-  ctx.drawImage(e.target, startPos.x, startPos.y);
+function init() {
+  for (let i = 1; i <= 64; i++) {
+    const img = new Image();
+    if (i < 10){
+      img.src = `./assets/000${i}.png`;
+    } else {
+      img.src = `./assets/00${i}.png`;
+    }
+    imgsArr.push(img);
+  }
 }
+
+init();
+
+let imgCount = 0;
 
 window.addEventListener('wheel', (e) => {
   console.log(e.deltaY);
+
+  if (e.deltaY > 0 && imgCount < 63) {
+    const startPos = calcDrawStartPos(imgsArr[imgCount]);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(imgsArr[imgCount], startPos.x, startPos.y);
+    imgCount++;
+  } else if (e.deltaY < 0 && imgCount > 0) {
+    imgCount--;
+    const startPos = calcDrawStartPos(imgsArr[imgCount]);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(imgsArr[imgCount], startPos.x, startPos.y);
+  }
 })
