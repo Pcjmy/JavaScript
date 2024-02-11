3d7fcd4a8621aeb3e12cc21aeca26484071e8985
Pcjmy <1716045191@qq.com>
2024-02-11 18:30:46 +0800

    feat: currentTime and duration

--- src/media/video/video-player/js/video.js
+++ src/media/video/video-player/js/video.js
@@ -1,8 +1,12 @@
 (function (doc) {
+  var t = null;
+
   var KSVideo = function (dom, opt) {
     this.videoBox = doc.getElementById(dom);
     this.vid = this.videoBox.getElementsByClassName('video-tag')[0];
     this.oPlayBtn = this.videoBox.getElementsByClassName('play-img')[0];
+    this.oCurrentTime = this.videoBox.getElementsByClassName('current-time')[0];
+    this.oDuration = this.videoBox.getElementsByClassName('duration')[0];
 
     console.log(this.videoBox);
 
@@ -21,6 +25,9 @@
     },
 
     bindEvent: function() {
+      this.vid.addEventListener('canplay', this._canplay.bind(this), false);
+      this.vid.addEventListener('playing', this._playing.bind(this), false);
+
       this.oPlayBtn.addEventListener('click', this.playVideo.bind(this), false);
     },
 
@@ -48,7 +55,77 @@
 
     setSrc: function() {
 
+    },
+
+    _canplay: function() {
+      setTime(this.oDuration, this.vid.duration);
+    },
+
+    _playing: function() {
+      var _self = this;
+      
+      t = setInterval(function() {
+        setTime(_self.oCurrentTime, _self.vid.currentTime);
+      }, 1000);
     }
+  }
+
+  function setTime (dom, time) {
+    dom.innerText = timeFormat(time);
+  }
+
+  function timeFormat (second) {
+    var h = parseInt(second / 3600);
+    var m = parseInt(parseInt(second % 3600) / 60);
+    var s = parseInt(second % 60);
+    time = '';
+    if (h == 0) {
+      if (m >= 10) {
+        if (s >= 10) {
+          time = '00:' + m + ':' + s;
+        } else {
+          time = '00:' + m + ':0' + s;
+        }
+      } else {
+        if (s >= 10) {
+          time = '00:0' + m + ':' + s;
+        } else {
+          time = '00:0' + m + ':0' + s;
+        }
+      }
+    } else {
+      if (h >= 10) {
+        if (m >= 10) {
+          if (s >= 10) {
+            time = '0' + h + ':' + m + ':' +s;
+          } else {
+            time = '0' + h + ':' + m + ':0' + s;
+          }
+        } else {
+          if (s >= 10) {
+            time = '0' + h + ':0' + m + ':' + s;
+          } else {
+            time = '0' + h + ':0' + m + ':0' + s;
+          }
+        }
+      } else {
+        if (m >= 10) {
+          if (s >= 10) {
+            time = h + ':' + m + ':' + s;
+          } else {
+            time = h + ':' + m + ':0' + s;
+          }
+        } else {
+          if (s >= 10) {
+            time = h + ':0' + m + ':' + s;
+          } else {
+            time = h + ':0' + m + ':0' + s;
+          }
+        }
+      }
+    }
+
+    return time;
   }
 
   window.KSVideo = KSVideo;
